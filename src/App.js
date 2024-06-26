db6880bacc2dd9b06549cfe2939dd520cc699956
Onyedikachi Obi <victorkhachi@gmail.com>
2024-06-26 09:17:06 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import PaystackPop from "@paystack/inline-js";
 function App() {
   const paystack = new PaystackPop();
   const key =  process.env.REACT_APP_PSKEY ;
-
+ 
   console.log(key);
   const location = new URLSearchParams(window.location.search);
+   const reidrect =()=>{
+   const button = document.createElement("a")
+   button.setAttribute("href",location.get("redirect"));
+   button.click()
+   }
   const fundWallet = () => {
     console.log(location.get('amount') * 100);
       paystack.newTransaction({
         amount: location.get('amount') * 100,
         email: location.get('email'),
         key: key,
-        onSuccess: () => alert("back to app")
+        onClose: reidrect(),
+        onCancel: reidrect(),
+        onSuccess:  reidrect()
       });
   };
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(()=>fundWallet(),[])
   return (
     <div className="fund auth">
-
+       
     </div>
   );
 }
